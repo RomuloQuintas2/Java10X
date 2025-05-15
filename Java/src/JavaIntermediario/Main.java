2b84d41a39b340b5b83039a61f25d40e20ee67da
romulo239 <romulofernando2391@gmail.com>
2025-05-15 16:17:37 -0300

    Initial commit

---
+++ Java/src/JavaIntermediario/Main.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Ninja Saske = new Ninja();
+
+        Saske.nome = "Saske Uchiha";
+        Saske.idade = 19;
+        Saske.aldeia = "Folha";
+    }
+}
