6eea88ed81b9de869c438e92d613dfc54a3e1d7f
quintas2 <romulofernando2391@gmail.com>
2025-05-25 16:48:41 -0300

    update #001

--- Java/src/JavaIntermediario/Main.java
+++ Java/src/JavaIntermediario/Main.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Ninja Saske = new Ninja();
-
+        //TSA
         Saske.nome = "Saske Uchiha";
         Saske.idade = 19;
         Saske.aldeia = "Folha";
